deb9bba5d8681c2690e5cba5515abe793b30b888
ian vetter <61299105+ianvet31@users.noreply.github.com>
2022-08-14 00:14:50 -0500

    Add files via upload

---
+++ hw5_main.py
@@ -0,0 +1,17 @@
+import hw5_utils
+import hw5_vae
+import sys
+import argparse
+import matplotlib.pyplot as plt
+plt.rcParams["axes.grid"] = False
+import matplotlib.image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from hw5_utils import array_to_image, concat_images, batch_indices, load_mnist
+
+
+
+hw5_vae.main()
